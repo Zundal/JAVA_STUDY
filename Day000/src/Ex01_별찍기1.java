2fa6ca4e1c38d3e3f2ea07e103327b654aded4c1
KwonSsohyun <thgus4982@naver.com>
2021-11-14 15:21:00 +0900

    별찍기 문제를 풀어보았습니다.

---
+++ Day000/src/Ex01_별찍기1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Star{
+	
+	// 생성자
+	Star(){
+		System.out.println("몇번째줄까지 차례대로 별을 출력하고 싶습니까?");
+	}
+	
+	public int sum() {
+		Scanner scan = new Scanner(System.in);
+		int str = scan.nextInt();
+		return str;
+	}
+	
+}
+
+public class Ex01_별찍기1 {
+
+	public static void main(String[] args) {
+		
+		Star star = new Star();
+		int strr = star.sum();
+		
+		for(int i = 0; i < strr; i++) {
+			System.out.print('*');
+			
+			for(int y = 1; y <= i; y++) {
+				System.out.print('*');
+			}
+			System.out.println(' ');
+		}
+
+	}
+
+}
